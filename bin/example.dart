22a62dbb2a69e9aadb43976debdff258c7ae2009
Hamzeh Athamneh <hamzehathamneh@desktop-62lk731-4.home>
2021-08-03 15:57:47 +0300

    create_person_class

--- bin/example.dart
+++ bin/example.dart
@@ -1,15 +1,31 @@
 void main() {
-  String compare(int num1, int num2) {
-    if (num1 > num2) {
-      return "$num1 is bigger than $num2";
-    } else if (num2 > num1) {
-      return "$num2 is bigger than $num1";
-    } else {
-      return "both numbers are equal";
-    }
+  Person hamzeh = new Person(
+    name: "Hamzeh",
+    age: 20,
+    job: "programmer",
+  );
+
+  print(hamzeh.name);
+  print(hamzeh.age);
+  print(hamzeh.job);
+}
+
+class Person {
+  String name = "";
+  int age = 0;
+  String job = "";
+
+  void sayHello() {
+    print("hello");
   }
 
-  String result = compare(66, 66);
-
-  print(result);
+  Person({
+    String name = "",
+    String job = "",
+    int age = 0,
+  }) {
+    this.name = name;
+    this.job = job;
+    this.age = age;
+  }
 }
